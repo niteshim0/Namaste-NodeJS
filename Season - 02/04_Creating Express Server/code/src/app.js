4f43eba59eef02f2cb675de3cf5b4f9b2ac3e4e7
niteshim0 <niteshim111@gmail.com>
2026-01-07 19:53:06 +0530

    for all the req same response

--- Season - 02/04_Creating Express Server/code/src/app.js
+++ Season - 02/04_Creating Express Server/code/src/app.js
@@ -3,4 +3,14 @@ const express = require("express");
 
 const app = express();
 
-app.listen(3000);
+// handling the incoming request
+// that is handling all the request coming from clients
+// this callback function is known as request handler
+app.use((req,res)=>{
+  res.send("Hello re clientwa : kaisa hai!")
+});
+
+// Server is listening
+app.listen(3000,()=>{
+  console.log("Server is listening on port : 3000");
+});
