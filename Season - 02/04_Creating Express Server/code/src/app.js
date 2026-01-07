a77b2565ffb0eeba0c4320ffb15f8a2dc1246508
niteshim0 <niteshim111@gmail.com>
2026-01-07 20:03:41 +0530

    server creation

--- Season - 02/04_Creating Express Server/code/src/app.js
+++ Season - 02/04_Creating Express Server/code/src/app.js
@@ -3,11 +3,20 @@ const express = require("express");
 
 const app = express();
 
+// just handling the port
+// this is the root route
+// it will not able to go in any other route because of this 
+// route is defined at the top
+// so this route will take the highest priority
+app.use("/",(req,res)=>{
+  res.send("Hello re clientwa : kaisa hai!")
+});
+
 // handling the incoming request
 // that is handling all the request coming from clients
 // this callback function is known as request handler
-app.use((req,res)=>{
-  res.send("Hello re clientwa : kaisa hai!")
+app.use("/test",(req,res)=>{
+  res.send("Hello re clientwa : kaisa hai! this is a test route")
 });
 
 // Server is listening
