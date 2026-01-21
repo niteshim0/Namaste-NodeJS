4fb202c7adde4d09e8423f4feed78801af2f2810
niteshim0 <niteshim111@gmail.com>
2026-01-22 02:04:02 +0530

    cookie,authentication and jwt

---
+++ Season - 02/11_Authentication,JWT and Cookies/notes/notes.js
@@ -0,0 +1,9 @@
+/*
+// Read about res.cookie at express.js framework
+// Read about cookie-parser
+// Read about req.cookies
+// Read about jwt
+// How to generate token and validate token
+// Try new jwt handbook (its brilliant)
+// Read about RFC
+*/
